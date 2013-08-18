8fa994b655c43731e5538f83056b73fcee8f1dae
mtw04 <mtw04b@gmail.com>
2013-08-18 15:57:32 -0700

    added MvcMusicStore

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            // Create a list of genres
+            List<string> genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+
+            // Create our view model
+            StoreIndexViewModel viewModel = new StoreIndexViewModel
+            {
+                NumberOfGenres = genres.Count(),
+                Genres = genres
+            };
+
+            return this.View(viewModel);
+        }
+
+        // GET: /Store/Browse
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+
+            return message;
+        }
+
+        // GET: /Store/Details  
+        public string Details(int id)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Details, ID = " + id);
+
+            return message;
+        }
+
+    }
+}
